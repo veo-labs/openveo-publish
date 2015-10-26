5707c32e09c12911f2d36132d5ff0f2d815796df
Maxime Béguin <maxime.beguin@vodalys-labs.com>
2015-10-26 16:24:45 +0100

    Auto-generated documentation for version 1.0.0

---
+++ 1.0.0/api/back-end/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "publishService"
+    ],
+    "modules": [
+        "ov.publish"
+    ],
+    "allModules": [
+        {
+            "displayName": "ov.publish",
+            "name": "ov.publish",
+            "description": "Defines a publish service to manage videos, watcher, categories and properties."
+        }
+    ]
+} };
+});
