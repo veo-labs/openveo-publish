f0af138944883617d81609a2353136eefa881e7b
Maxime Béguin <maxime.beguin@vodalys-labs.com>
2015-11-23 16:11:34 +0100

    Add translation for media availability message on player
    Availability error message wasn't translated at all. There is now a translation for both english and french.

--- app/client/front/js/PlayerController.js
+++ app/client/front/js/PlayerController.js
@@ -5,7 +5,7 @@
   /**
    * Defines the player controller.
    */
-  function PlayerController($scope, $window, $location, videoService) {
+  function PlayerController($scope, $window, $location, $sce, videoService, ovPublishTranslations) {
     $scope.ready = false;
 
     var urlChunks = /.*\/([^\/]*)\/?/.exec($location.path());
@@ -16,16 +16,19 @@
 
       videoService.getVideo(mediaId, function(video) {
         if (video) {
+
+          // Retrieve url parameters
+          var urlParams = $location.search();
           $scope.data = video;
+          $scope.language = urlParams['lang'] || navigator.language || navigator.browserLanguage;
+          $scope.publishLanguage = ovPublishTranslations[$scope.language] ? angular.copy($scope.language) : 'en';
+
+          var dictionary = ovPublishTranslations[$scope.publishLanguage];
+          $scope.availabilityMessage = $sce.trustAsHtml(dictionary['AVAILABILITY_MESSAGE']);
 
           if (video.available) {
-
-            // Retrieve url parameters
-            var urlParams = $location.search();
-
             $scope.isFullViewport = urlParams['fullscreen'] || false;
             $scope.playerType = urlParams['type'] || 'html';
-            $scope.language = urlParams['lang'] || navigator.language || navigator.browserLanguage;
             $scope.ready = true;
           }
         }
@@ -37,6 +40,6 @@
   }
 
   app.controller('PlayerController', PlayerController);
-  PlayerController.$inject = ['$scope', '$window', '$location', 'videoService'];
+  PlayerController.$inject = ['$scope', '$window', '$location', '$sce', 'videoService', 'ovPublishTranslations'];
 
 })(angular.module('ov.publish.player'));
--- app/client/front/js/PublishPlayerApp.js
+++ app/client/front/js/PublishPlayerApp.js
@@ -11,4 +11,15 @@
     $locationProvider.html5Mode(true);
   }]);
 
+  // Translations
+  app.constant('ovPublishTranslations', {
+    en: {
+      AVAILABILITY_MESSAGE: 'This video is not yet available on the streaming platform.<br/>Please retry later.'
+    },
+    fr: {
+      AVAILABILITY_MESSAGE: 'Cette vidéo n\'est pas encore disponible sur la plateforme vidéo.<br/>' +
+      'Veuillez réessayer plus tard.'
+    }
+  });
+
 })(angular);
