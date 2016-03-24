6424e13488a58f4ce60537f6c05bfa0dc73273c2
Maxime Béguin <maxime.beguin@veo-labs.com>
2016-03-24 15:31:54 +0100

    Implement init method and change start method due to openveo-core modifications
    openveo-core now calls the Plugin.init method of each plugin when database and plugins are available, it offers the possibility to create database collections' indexes. Indexes are now created for both properties and videos' collections when the application start.
    The Plugin.start method was synchronous, it is now asynchronous.
    The Plugin.start method was only called for the application, not the Web Service process, it is now called for each process. Difference between Web Service and application processes can be checked using the process.isWebService property.

--- app/server/PublishPlugin.js
+++ app/server/PublishPlugin.js
@@ -6,11 +6,13 @@
  * @module publish-plugin
  */
 
-// Module dependencies
 var util = require('util');
 var express = require('express');
+var async = require('async');
 var openVeoAPI = require('@openveo/api');
 var watcherManager = process.requirePublish('app/server/watcher/watcherManager.js');
+var PropertyProvider = process.requirePublish('app/server/providers/PropertyProvider.js');
+var VideoProvider = process.requirePublish('app/server/providers/VideoProvider.js');
 
 /**
  * Creates a PublishPlugin.
@@ -53,13 +55,49 @@ module.exports = PublishPlugin;
 util.inherits(PublishPlugin, openVeoAPI.Plugin);
 
 /**
+ * Prepares plugin by creating required database indexes.
+ *
+ * This is automatically called by core application after plugin is loaded.
+ *
+ * @method init
+ * @async
+ * @param {Function} callback Function to call when it's done with :
+ *  - **Error** An error if something went wrong, null otherwise
+ */
+PublishPlugin.prototype.init = function(callback) {
+  var database = openVeoAPI.applicationStorage.getDatabase();
+  var asyncFunctions = [];
+  var providers = [
+    new PropertyProvider(database),
+    new VideoProvider(database)
+  ];
+
+  providers.forEach(function(provider) {
+    if (provider.createIndexes) {
+      asyncFunctions.push(function(callback) {
+        provider.createIndexes(callback);
+      });
+    }
+  });
+
+  async.parallel(asyncFunctions, function(error, results) {
+    callback(error);
+  });
+};
+
+/**
  * Starts the watcher when plugin is ready.
  *
- * This is automatically called by core application after plugin is
- * loaded.
+ * This is automatically called by core application after plugin is initialized.
  *
  * @method start
+ * @async
+ * @param {Function} callback Function to call when it's done with :
+ *  - **Error** An error if something went wrong, null otherwise
  */
-PublishPlugin.prototype.start = function() {
-  watcherManager.start();
+PublishPlugin.prototype.start = function(callback) {
+  if (!process.isWebService)
+    watcherManager.start();
+
+  callback();
 };
--- app/server/providers/PropertyProvider.js
+++ app/server/providers/PropertyProvider.js
@@ -21,3 +21,22 @@ function PropertyProvider(database) {
 
 module.exports = PropertyProvider;
 util.inherits(PropertyProvider, openVeoAPI.EntityProvider);
+
+/**
+ * Creates properties indexes.
+ *
+ * @method createIndexes
+ * @async
+ * @param {Function} callback Function to call when it's done with :
+ *  - **Error** An error if something went wrong, null otherwise
+ */
+PropertyProvider.prototype.createIndexes = function(callback) {
+  this.database.createIndexes(this.collection, [
+    {key: {name: 'text', description: 'text'}, weights: {name: 2}, name: 'querySearch'}
+  ], function(error, result) {
+    if (result && result.note)
+      process.logger.debug('Create properties indexes : ' + result.note);
+
+    callback(error);
+  });
+};
