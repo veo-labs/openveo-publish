3a5d6afc383d1eeb80faa9a22e12859cbf31e5ff
Maxime Béguin <maxime.beguin@veo-labs.com>
2017-09-12 17:13:29 +0200

    Auto-generated documentation for version 4.0.0

---
+++ 4.0.0/api/server/api.js
@@ -0,0 +1,83 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ConfigurationController",
+        "ConfigurationModel",
+        "ConfigurationProvider",
+        "DirectoryFsWatcher",
+        "DirectoryWatcher",
+        "ERRORS",
+        "GoogleOAuthHelper",
+        "HTTP_ERRORS",
+        "LocalProvider",
+        "Package",
+        "PackageError",
+        "PropertyController",
+        "PropertyModel",
+        "PropertyProvider",
+        "PublishError",
+        "PublishManager",
+        "PublishPlugin",
+        "ResumableUpload",
+        "STATES",
+        "StatisticsController",
+        "TYPES",
+        "TarPackage",
+        "TarPackageError",
+        "VideoController",
+        "VideoModel",
+        "VideoPackage",
+        "VideoPackageError",
+        "VideoPlatformProvider",
+        "VideoProvider",
+        "VimeoProvider",
+        "Watcher",
+        "WatcherError",
+        "WowzaProvider",
+        "YoutubeProvider",
+        "factory",
+        "videoPlatformFactory"
+    ],
+    "modules": [
+        "controllers",
+        "models",
+        "packages",
+        "providers",
+        "publish",
+        "watcher"
+    ],
+    "allModules": [
+        {
+            "displayName": "controllers",
+            "name": "controllers",
+            "description": "Include all plugin's controllers to handle HTTP or socket messages."
+        },
+        {
+            "displayName": "models",
+            "name": "models",
+            "description": "Publish models."
+        },
+        {
+            "displayName": "packages",
+            "name": "packages",
+            "description": "All packages types which can be processed."
+        },
+        {
+            "displayName": "providers",
+            "name": "providers",
+            "description": "All publish providers."
+        },
+        {
+            "displayName": "publish",
+            "name": "publish",
+            "description": "Defines the Publish Plugin that will be loaded by the core application."
+        },
+        {
+            "displayName": "watcher",
+            "name": "watcher",
+            "description": "Defines a Watcher to be aware of new resources added to directories."
+        }
+    ],
+    "elements": []
+} };
+});
