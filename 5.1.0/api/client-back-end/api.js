e81bde5e49382fc07f01be77577d98b2cd80231f
Paul-Antoine GUYON <paul-antoine.guyon@veo-labs.com>
2018-02-23 15:39:32 +0100

    Auto-generated documentation for version 5.1.0

---
+++ 5.1.0/api/client-back-end/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "publishService"
+    ],
+    "modules": [
+        "ov.publish"
+    ],
+    "allModules": [
+        {
+            "displayName": "ov.publish",
+            "name": "ov.publish",
+            "description": "Defines a publish service to manage medias, categories and properties."
+        }
+    ],
+    "elements": []
+} };
+});
