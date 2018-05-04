f2f786f9ab608a09e09d2d8eef09a76d08d87a44
Maxime Béguin <maxime.beguin@veo-labs.com>
2018-05-04 16:55:21 +0200

    Auto-generated documentation for version 6.0.0

---
+++ 6.0.0/api/client-back-end/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "millisecondsToTime",
+        "ovpTime",
+        "publishService",
+        "timeToMilliseconds"
+    ],
+    "modules": [
+        "ov.publish"
+    ],
+    "allModules": [
+        {
+            "displayName": "ov.publish",
+            "name": "ov.publish",
+            "description": "Convert time in milliseconds into time string (hh:mm:ss)"
+        }
+    ],
+    "elements": []
+} };
+});
